78d40ef3d909e8356180ea5d44fe397ec2b8d4d6
Balieiro004 <Balieiro004@gmail.com>
2024-11-24 15:06:41 -0300

    Adiconando listagem paginada na busca dos clientes

--- src/main/java/com/balieiro/crudClientes/controller/ClientController.java
+++ src/main/java/com/balieiro/crudClientes/controller/ClientController.java
@@ -2,12 +2,17 @@ package com.balieiro.crudClientes.controller;
 
 
 import com.balieiro.crudClientes.dto.ClientDTO;
+import com.balieiro.crudClientes.entities.Client;
 import com.balieiro.crudClientes.service.ClientService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -19,5 +24,10 @@ public class ClientController {
     @GetMapping(value = "/{id}")
     public ClientDTO findById(@PathVariable Long id) {
         return clientService.findById(id);
+    }
+
+    @GetMapping
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        return clientService.findAll(pageable);
     }
 }
--- src/main/java/com/balieiro/crudClientes/service/ClientService.java
+++ src/main/java/com/balieiro/crudClientes/service/ClientService.java
@@ -4,8 +4,12 @@ import com.balieiro.crudClientes.dto.ClientDTO;
 import com.balieiro.crudClientes.entities.Client;
 import com.balieiro.crudClientes.repositories.ClientRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @AllArgsConstructor
@@ -18,5 +22,11 @@ public class ClientService {
     public ClientDTO findById(Long id) {
         Client client = clientRepository.findById(id).get();
         return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = clientRepository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
     }
 }
